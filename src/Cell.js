bd61b65d1bf3e88a7472b0dbc1e9d0e16f791c75
AyyLam <al3101@nyu.edu>
2018-07-16 17:56:32 -0400

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// import {value} from './Matrix.js';
+
+ class Cell extends React.Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       color: this.props.value
+     }
+   }
+
+   changeColor = () => {
+     this.setState({
+       color: '#333'
+     })
+   }
+   render() {
+     return(
+       <div
+       className="cell"
+       onClick={this.changeColor}
+       style={{backgroundColor: this.state.color}}>
+       </div>
+     )
+   }
+}
+
+export default Cell
